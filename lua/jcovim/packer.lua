44059761be18f41c95ad814c8ae294dd42ff0571
james <jcotab@gmail.com>
2023-03-01 22:36:10 -0700

    .

--- lua/jcovim/packer.lua
+++ lua/jcovim/packer.lua
@@ -70,4 +70,11 @@ return require('packer').startup(function(use)
     requires = "nvim-lua/plenary.nvim",
 }
 
+use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+})
+
+use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+
 end)
